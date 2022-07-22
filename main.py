07d52d2e00b844485389b493922cc7ca2f5268dc
AndereLion <senjaskorp@gmail.com>
2022-07-22 11:08:46 +0200

    ...

--- main.py
+++ main.py
@@ -788,3 +788,27 @@ import math
 # print(xo("ooxXm"))
 # print(xo("zpzpzpp"))
 # print(xo(("zzoo")))
+############################
+# Given an array of integers, return a new array with each value doubled.
+#
+# For example:
+#
+# [1, 2, 3] --> [2, 4, 6]
+# def maps(a):
+#     return [i * 2 for i in a]
+#
+# print(maps([1, 2, 3]))
+###########################
+# Complete the square sum function so that it squares each number passed into it and then sums the results together.
+#
+# For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+# def square_sum(numbers):
+#     return sum([i ** 2 for i in numbers])
+#
+# print(square_sum([1, 2, 2]))
+#######################################
+# def no_space(x):
+#     return x.replace(' ', '')
+#
+# print(no_space('dfgdgdg dgdgdd'))
+#######################################
